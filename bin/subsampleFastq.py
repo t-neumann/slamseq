98bc0b86438b9d4e963bd8b5590aadfe865ec1e4
Tobias Neumann <tobias.neumann.at@gmail.com>
2020-04-24 09:47:33 +0200

    utility script do extract reads from fastq file

---
+++ bin/subsampleFastq.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2020 Tobias Neumann
+
+from __future__ import print_function
+import sys
+
+import Bio
+import Bio.SeqIO
+from Bio.Seq import Seq
+
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter, SUPPRESS
+
+ # Info
+usage = "Subsample fastq file"
+version = "1.0"
+
+# Main Parsers
+parser = ArgumentParser(description=usage)
+
+parser.add_argument("-f", "--fastq", type=str, required=True, dest="fastqFile", help="Fastq")
+parser.add_argument("-i", "--ids", type=str, required=True, dest="idsFile", help="File with read IDs")
+
+args = parser.parse_args()
+
+for record in Bio.SeqIO.parse(args.fastqFile, 'fastq'):
+    readName = str(record.id)
+    print(record)
